mobygit/dwl

commit 72a7d78a1a7926a207539eb50f44b2e1eb089d49

make the borders red borders for urgent clients
iirc this is the same behavior of dwm

dwl.c
@@ -2589,9 +2589,13 @@ urgent(struct wl_listener *listener, void *data)
 {
 	struct wlr_xdg_activation_v1_request_activate_event *event = data;
 	Client *c = NULL;
+	int i;
 	toplevel_from_wlr_surface(event->surface, &c, NULL);
 	if (!c || c == focustop(selmon))
 		return;
+
+	for (i = 0; i < 4; i++)
+		wlr_scene_rect_set_color(c->border[i], urgentcolor);
 
 	c->isurgent = 1;
 	printstatus();
@@ -2752,8 +2756,12 @@ void
 sethints(struct wl_listener *listener, void *data)
 {
 	Client *c = wl_container_of(listener, c, set_hints);
+	int i;
 	if (c == focustop(selmon))
 		return;
+
+	for (i = 0; i < 4; i++)
+		wlr_scene_rect_set_color(c->border[i], urgentcolor);
 
 	c->isurgent = xcb_icccm_wm_hints_get_urgency(c->surface.xwayland->hints);
 	printstatus();

config.def.h
@@ -9,6 +9,7 @@ static const int bypass_surface_visibility = 0;  /* 1 means idle inhibitors will
 static const unsigned int borderpx         = 1;  /* border pixel of windows */
 static const float bordercolor[]           = COLOR(0x444444ff);
 static const float focuscolor[]            = COLOR(0x005577ff);
+static const float urgentcolor[]           = COLOR(0xff0000ff);
 /* To conform the xdg-protocol, set the alpha to zero to restore the old behavior */
 static const float fullscreen_bg[]         = {0.1, 0.1, 0.1, 1.0}; /* You can also use glsl colors */
 

commit 43d30256db6dc4b224e14084aa7663eb85d6875f

only set border color for urgent *and* mapped X11 clients
this fixes a segfault when the client emits .set_hints but it's not mapped

Fixes: 72a7d78a1a7926a207539eb50f44b2e1eb089d49

dwl.c
@@ -2780,8 +2780,11 @@ sethints(struct wl_listener *listener, void *data)
 	if (c == focustop(selmon))
 		return;
 
-	client_set_border_color(c, urgentcolor);
 	c->isurgent = xcb_icccm_wm_hints_get_urgency(c->surface.xwayland->hints);
+
+	if (c->isurgent && client_is_mapped(c))
+		client_set_border_color(c, urgentcolor);
+
 	printstatus();
 }
 